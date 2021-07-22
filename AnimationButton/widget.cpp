51274d01907a13bc28f4fa755ab6470f24e0a06d
liuhongwei <liuhongwei@zhuanzhuan.com>
2021-07-22 17:35:11 +0800

    1

---
+++ AnimationButton/widget.cpp
@@ -0,0 +1,21 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QPropertyAnimation>
+#include <QSequentialAnimationGroup>
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->pushButton->setAnimationEnable(true);
+    ui->pushButton->setContext("Wo ai ni ppp...");
+    ui->pushButton->setAlignment(Qt::AlignCenter);
+    
+    ui->pushButton_2->setContext("Me to me too...");
+    ui->pushButton_2->setAlignment(Qt::AlignHCenter|Qt::AlignBottom);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
